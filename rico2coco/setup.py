746e2582f8916e584470b62c6b6a3d014bc02292
xrhd <rhd@icomp.ufam.edu.br>
2021-08-01 18:43:48 -0400

    refactor: format files

--- rico2coco/setup.py
+++ rico2coco/setup.py
@@ -1,24 +1,23 @@
 from setuptools import setup
 
-packages = ['rico2coco']
+packages = ["rico2coco"]
 
-package_data = {'': ['*']}
+package_data = {"": ["*"]}
 
 setup_kwargs = {
-    'name': 'rico2coco',
-    'version': '0.1.0',
-    'description': '',
-    'long_description': None,
-    'author': 'xrhd',
-    'author_email': 'rhd@icomp.ufam.edu.br',
-    'maintainer': None,
-    'maintainer_email': None,
-    'url': None,
-    'packages': packages,
-    'package_data': package_data,
-    'python_requires': '>=3.8,<4.0',
+    "name": "rico2coco",
+    "version": "0.1.0",
+    "description": "",
+    "long_description": None,
+    "author": "xrhd",
+    "author_email": "rhd@icomp.ufam.edu.br",
+    "maintainer": None,
+    "maintainer_email": None,
+    "url": None,
+    "packages": packages,
+    "package_data": package_data,
+    "python_requires": ">=3.8,<4.0",
 }
 
 
 setup(**setup_kwargs)
-
