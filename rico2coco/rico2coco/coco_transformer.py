f0330d869285fc927a153cc04368c19cd25c179e
xrhd <rhd@icomp.ufam.edu.br>
2021-08-07 17:25:32 -0400

    feat: basic coco transformer

---
+++ rico2coco/rico2coco/coco_transformer.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+from rico2coco.metadata import rico_metadata
+
+def get_info():
+    return {
+        "year": "2021",
+        "version": "1.0",
+        "description": "Custum Rico Dataset in Coco format.",
+        "contributor": "xrhd",
+        "url": "",
+        "date_created": ""
+    }
+
+def get_licenses():
+    return [
+        {}
+    ]
+
+def get_categories(component_legend: pd.DataFrame = rico_metadata.component_legend):
+    for i, label_name in enumerate(component_legend):
+        yield {"supercategory": "none", "id": i + 1, "name": label_name}
+
+
+def get_images(ui_details: pd.DataFrame = rico_metadata.ui_detail):
+    for ui_id in ui_details["UI Number"]:
+        # image
+        img_id = int(ui_id) + 1
+        img_name = f"{ui_id}.jpg"
+
+        width, height = 1080, 1920  # img.size
+        yield {"id": img_id, "height": height, "width": width, "file_name": img_name}
+
+
+def get_annotations(semantic_annotation: dict):
+    pass
