0b85da4a29b1400231f817a6ac02573deaa7f001
xrhd <rhd@icomp.ufam.edu.br>
2021-09-05 14:28:40 -0400

    refactor: rico2coco2yolo

--- yolov5/rico2coco2yolo.py
+++ yolov5/rico2coco2yolo.py
@@ -1,12 +1,13 @@
 import os
 import os.path
-
 from shutil import copyfile
+
 from pycocotools.coco import COCO
 
+USE_SYMLINK = True
 COCO_ANNOTATIONS = "../dataset/ricoco_clickable.json"
 IMAGES_PATH = "../rico2coco/rico/dataset/combined/"
-OUTPUT_PATH = "datasets/"
+OUTPUT_PATH = "datasets/rico2coco_clickable/"
 
 
 # Truncates numbers to N decimals
@@ -15,40 +16,42 @@ def truncate(n, decimals=0):
     return int(n * multiplier) / multiplier
 
 
-def main(coco_annotations):
+def main(coco_annotations, image_path, output_path, use_symlink=True):
     coco = COCO(coco_annotations)
     cats = coco.loadCats(coco.getCatIds())
-    nms=[cat['name'] for cat in cats]
-    print('COCO categories: \n{}\n'.format(' '.join(nms)))
+    nms = [cat["name"] for cat in cats]
+    print("COCO categories: \n{}\n".format(" ".join(nms)))
 
     catIds = coco.getCatIds()
-    imgIds = coco.getImgIds(catIds=catIds )
+    imgIds = coco.getImgIds(catIds=catIds)
     images = coco.loadImgs(imgIds)
     print(catIds, imgIds)
 
     # This creates a symbolic link on python in tmp directory
-    output_image_path = f"{OUTPUT_PATH}/images/"
+    output_image_path = f"{output_path}/images/"
     os.makedirs(output_image_path, exist_ok=True)
 
     for image in images:
         file_name = image["file_name"]
-        src = f"{IMAGES_PATH}/{file_name}"
+        src = f"{image_path}/{file_name}"
         dst = f"{output_image_path}/{file_name}"
-        if not(os.path.exists(dst) or os.path.islink(dst)):
-            # os.symlink(src, dst)
-            copyfile(src, dst)
+        if not (os.path.exists(dst) or os.path.islink(dst)):
+            if use_symlink:
+                os.symlink(src, dst)
+            else:
+                copyfile(src, dst)
 
     # This is where the annotations will be saved in YOLO format
-    output_label_path = f"{OUTPUT_PATH}/labels/"
+    output_label_path = f"{output_path}/labels/"
     os.makedirs(output_label_path, exist_ok=True)
     for im in images:
-        dw = 1. / im['width']
-        dh = 1. / im['height']
-        
-        annIds = coco.getAnnIds(imgIds=im['id'], catIds=catIds, iscrowd=None)
+        dw = 1.0 / im["width"]
+        dh = 1.0 / im["height"]
+
+        annIds = coco.getAnnIds(imgIds=im["id"], catIds=catIds, iscrowd=None)
         anns = coco.loadAnns(annIds)
-        
-        filename = im['file_name'].replace(".jpg", ".txt")
+
+        filename = im["file_name"].replace(".jpg", ".txt")
         print(filename)
 
         with open(output_label_path + filename, "a") as myfile:
@@ -57,20 +60,29 @@ def main(coco_annotations):
                 ymin = anns[i]["bbox"][1]
                 xmax = anns[i]["bbox"][2] + anns[i]["bbox"][0]
                 ymax = anns[i]["bbox"][3] + anns[i]["bbox"][1]
-                
-                x = (xmin + xmax)/2
-                y = (ymin + ymax)/2
-                
+
+                x = (xmin + xmax) / 2
+                y = (ymin + ymax) / 2
+
                 w = xmax - xmin
-                h = ymax-ymin
-                
+                h = ymax - ymin
+
                 x = x * dw
                 w = w * dw
                 y = y * dh
                 h = h * dh
-                
+
                 # Note: This assumes a single-category dataset, and thus the "0" at the beginning of each line.
-                mystring = str("0 " + str(truncate(x, 7)) + " " + str(truncate(y, 7)) + " " + str(truncate(w, 7)) + " " + str(truncate(h, 7)))
+                mystring = str(
+                    "0 "
+                    + str(truncate(x, 7))
+                    + " "
+                    + str(truncate(y, 7))
+                    + " "
+                    + str(truncate(w, 7))
+                    + " "
+                    + str(truncate(h, 7))
+                )
                 myfile.write(mystring)
                 myfile.write("\n")
 
@@ -78,4 +90,4 @@ def main(coco_annotations):
 
 
 if __name__ == "__main__":
-    main(COCO_ANNOTATIONS)
+    main(COCO_ANNOTATIONS, IMAGES_PATH, OUTPUT_PATH, USE_SYMLINK)
